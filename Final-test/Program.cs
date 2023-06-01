ac81709058057459b6bad89c9905222aff9e9493
Timoleks <timoleks@gmail.com>
2023-06-01 20:37:09 +0300

    small fix

--- Final-test/Program.cs
+++ Final-test/Program.cs
@@ -1,115 +1,105 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
-namespace UdpServer
+namespace UdpServer;
+
+class Program
 {
+    private static Dictionary<IPEndPoint, Client> _clients = new();
 
-    class Program
+    public static async Task Main(string[] args)
     {
-        private static Dictionary<IPEndPoint, Client> _clients = new();
+        using var udpServer = new UdpClient(5000);
+        TimeSpan timeout = TimeSpan.FromSeconds(20);
 
+        Console.WriteLine("udp started with 5000 port");
 
-        public static async Task Main(string[] args)
+        try
         {
-            using var udpServer = new UdpClient(5000);
-            TimeSpan timeout = TimeSpan.FromSeconds(20);
-
-            Console.WriteLine("udp started with 5000 port");
-
-            try
-            {
-                ClientRemove(timeout); 
-                while (true)
-                {
-                    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
-                    var receivedData = udpServer.Receive(ref remoteEP);
-                    var received = Encoding.UTF8.GetString(receivedData);
-                    ClientsDataReceiveing(udpServer, timeout, remoteEP, received);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadKey();
-        }
-
-        private static async Task ClientsDataReceiveing(UdpClient udpServer, TimeSpan timeout, IPEndPoint remoteEP, string received)
-        {
-            try
-            {
-                Client? client;
-                var clientExists = _clients.TryGetValue(remoteEP, out client);
-
-                if (received == "Confirmed" && clientExists)
-                {
-                    client!.Received = true;
-
-                    return;
-                }
-
-                if (clientExists)
-                {
-                    client!.Count++;
-                    client.Received = false;
-                    client.lastReceiveTime = DateTime.Now;
-                    DataSend(udpServer, remoteEP, received);
-                    return;
-                }
-
-                _clients[remoteEP] = new Client(remoteEP);
-                _clients[remoteEP].lastReceiveTime = DateTime.Now;
-
-                DataSend(udpServer, remoteEP, received);
-               
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        
-
-        private static async Task ClientRemove(TimeSpan timeout)
-        {
+            ClientRemove(timeout); 
             while (true)
             {
-                foreach (var client in _clients.Values)
-                {
-                    if (DateTime.Now - client.lastReceiveTime > timeout)
-                    {
-                        Console.WriteLine($"client {client.IpAdress} removed due to inactivity");
-
-                        if (_clients.Remove(client.IpAdress))
-                        {
-                            Console.WriteLine("Client removed");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Client not found");
-                        }
-                    }
-                   
-                }
-                await Task.Delay(1000);
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                var receivedData = udpServer.Receive(ref remoteEP);
+                var received = Encoding.UTF8.GetString(receivedData);
+                ClientsDataReceiveing(udpServer, timeout, remoteEP, received);
             }
-            
         }
-
-        private static void DataSend(UdpClient udpServer, IPEndPoint remoteEP, string received)
+        catch (Exception e)
         {
-            Console.WriteLine($"Data - \"{received}\" received from " + remoteEP.ToString());
-            var response = Encoding.UTF8.GetBytes(_clients[remoteEP].Count.ToString());
-            udpServer.Send(response, response.Length, remoteEP);
+            Console.WriteLine(e.Message);
         }
 
-
-
+        Console.ReadKey();
     }
 
+    private static async Task ClientsDataReceiveing(UdpClient udpServer, TimeSpan timeout, IPEndPoint remoteEP, string received)
+    {
+        try
+        {
+            Client? client;
+            var clientExists = _clients.TryGetValue(remoteEP, out client);
+
+            if (received == "Confirmed" && clientExists)
+            {
+                client!.Received = true;
+
+                return;
+            }
+
+            if (clientExists)
+            {
+                client!.Count++;
+                client.Received = false;
+                client.lastReceiveTime = DateTime.Now;
+                DataSend(udpServer, remoteEP, received);
+                return;
+            }
+
+            _clients[remoteEP] = new Client(remoteEP);
+            _clients[remoteEP].lastReceiveTime = DateTime.Now;
+
+            DataSend(udpServer, remoteEP, received);
+               
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static async Task ClientRemove(TimeSpan timeout)
+    {
+        while (true)
+        {
+            foreach (var client in _clients.Values)
+            {
+                if (DateTime.Now - client.lastReceiveTime > timeout)
+                {
+                    Console.WriteLine($"client {client.IpAdress} removed due to inactivity");
+
+                    if (_clients.Remove(client.IpAdress))
+                    {
+                        Console.WriteLine("Client removed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Client not found");
+                    }
+                }
+                   
+            }
+            await Task.Delay(1000);
+        }
+            
+    }
+
+    private static void DataSend(UdpClient udpServer, IPEndPoint remoteEP, string received)
+    {
+        Console.WriteLine($"Data - \"{received}\" received from " + remoteEP.ToString());
+        var response = Encoding.UTF8.GetBytes(_clients[remoteEP].Count.ToString());
+        udpServer.Send(response, response.Length, remoteEP);
+    }
 }
